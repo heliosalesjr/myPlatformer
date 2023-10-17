1246ed69328d35ccfd8c0f61a5220c3c0cf14fa0
heliosalesjr <heliosalesjr@gmail.com>
2023-10-17 20:41:57 -0300

    player movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -6,6 +6,11 @@ class_name Player
 @onready var animation_player = $AnimationPlayer
 
 const GRAVITY: float = 1000.0
+const RUN_SPEED: float = 120.0
+const MAX_FALL: float = 400.0
+const HURT_TIME: float = 0.3
+const JUMP_VELOCITY: float = -400.0
+
 
 func _ready():
 	pass
@@ -13,5 +18,16 @@ func _ready():
 func _physics_process(delta):
 	if is_on_floor() == false:
 		velocity.y += GRAVITY * delta
-	
+	get_input()
 	move_and_slide()
+
+func get_input() -> void:
+	velocity.x = 0
+	if Input.is_action_pressed("left"):
+		velocity.x = -RUN_SPEED
+	if Input.is_action_pressed("right"):
+		velocity.x = RUN_SPEED
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
+		
